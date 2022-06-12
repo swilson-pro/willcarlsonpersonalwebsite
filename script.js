0a95670e06c2d22be4a4429efc0a2c2ce52007d1
Will <swilson.carlson@hotmail.com>
2022-06-12 14:48:25 -0400

    added javascript events for mouseover, added a comment about this prework assignment on bottom

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This was a great pre-work assignment!";
 
 document.querySelector("body").appendChild(h2);
+
+const box = document.getElementById('topHead');
+
+// changes color of text when mousover
+heading.addEventListener('mouseover', function handleMouseOver() {
+  heading.style.color = 'yellow';
+});
+
+// changes the color of text when mouseout
+heading.addEventListener('mouseout', function handleMouseOut() {
+  heading.style.color = 'red';
+});
+
